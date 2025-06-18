0baba8d018d63ba87cd580c53a717b1b92925c44
Fijxu <fijxu@nadeko.net>
2025-06-18 19:47:32 -0400

    refactor+fix: move simple statements to process and fix the renaming of chatterino uploaded files

--- src/operations/upload.cr
+++ src/operations/upload.cr
@@ -11,57 +11,10 @@ module OP
       @fileinfo.uploaded_at = Time.utc.to_unix
     end
 
-    private def chatterino_filename? : Nil
-      if @fileinfo.filename == "control_v.png"
-        @fileinfo.original_filename = @fileinfo.filename
-      end
-    end
-
-    private def set_file_info : Nil
-      if filename = @uploaded_file.filename
-        @fileinfo.original_filename = filename
-      else
-        LOGGER.debug "No file provided by the user"
-        raise NoFileProvided.new
-      end
-
-      @fileinfo.filename = Utils.generate_filename
-
-      chatterino_filename?()
-      set_extension()
-    end
-
-    private def set_extension : Nil
-      @fileinfo.extension = File.extname("#{@uploaded_file.filename}")
-
-      validate_extension()
-    end
-
-    private def validate_extension : Nil
-      # Allow uploads without extension
-      if !@fileinfo.extension.empty?
-        if CONFIG.blocked_extensions.includes?(@fileinfo.extension.split(".")[1])
-          raise ExtensionNotAllowed.new(@fileinfo.extension)
-        end
-      end
-    end
-
     private def set_ip_information : Nil
       @fileinfo.ip = @ip_addr.to_s
       @ip.ip = @ip_addr.to_s
       @ip.date = @fileinfo.uploaded_at
-    end
-
-    private def generate_delete_key : Nil
-      if CONFIG.delete_key_length > 0
-        @fileinfo.delete_key = Random.base58(CONFIG.delete_key_length)
-      end
-    end
-
-    private def generate_checksum(file_path : String) : Nil
-      if CONFIG.enable_checksums
-        @fileinfo.checksum = Utils::Hashing.hash_file(file_path)
-      end
     end
 
     private def writefile(file_path : String) : Nil
@@ -79,6 +32,12 @@ module OP
           IO.copy(@uploaded_file.body, output)
         end
         generate_checksum(file_path)
+      end
+    end
+
+    private def generate_checksum(file_path : String) : Nil
+      if CONFIG.enable_checksums
+        @fileinfo.checksum = Utils::Hashing.hash_file(file_path)
       end
     end
 
@@ -102,16 +61,40 @@ module OP
     end
 
     def process : Nil
-      set_file_info()
+      if filename = @uploaded_file.filename
+        @fileinfo.original_filename = filename
+      else
+        LOGGER.debug "No file provided by the user"
+        raise NoFileProvided.new
+      end
+
+      @fileinfo.filename = Utils.generate_filename
+
+      # control_v.png and control_v.gif are filenames that are used for files
+      # uploaded using Chatterino, so we change the original filename to the
+      # randomly generated one.
+      if ["control_v.png", "control_v.gif"].includes?(@fileinfo.original_filename)
+        @fileinfo.original_filename = @fileinfo.filename
+      end
+
+      @fileinfo.extension = File.extname("#{@uploaded_file.filename}")
+
+      # Allow uploads without extension
+      if !@fileinfo.extension.empty?
+        if CONFIG.blocked_extensions.includes?(@fileinfo.extension.split(".")[1])
+          raise ExtensionNotAllowed.new(@fileinfo.extension)
+        end
+      end
 
       full_filename = @fileinfo.filename + @fileinfo.extension
       file_path = "#{CONFIG.files}/#{full_filename}"
 
-      validate_extension()
-
       writefile(file_path)
       set_ip_information()
-      generate_delete_key()
+
+      if CONFIG.delete_key_length > 0
+        @fileinfo.delete_key = Random.base58(CONFIG.delete_key_length)
+      end
 
       generate_thumbnail()
       insert_into_db()
